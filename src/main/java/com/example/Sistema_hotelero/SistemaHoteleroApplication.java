8db89ff7f56bd2ebbba4647635228ab3b369b636
Valentina Peña <valentykar@live.com>
2024-08-16 15:55:08 -0500

    primer commit

---
+++ src/main/java/com/example/Sistema_hotelero/SistemaHoteleroApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistema_hotelero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHoteleroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaHoteleroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sistema_hotelero/SistemaHoteleroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sistema_hotelero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaHoteleroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
